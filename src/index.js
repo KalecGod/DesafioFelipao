6cbd2d0f4a9b6c066b3906a16984f6eb4e139c59
KalecGod <adalbertomc123@gmail.com>
2025-03-07 20:09:55 -0300

    Faltando a ultima missao

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ let mana;
 console.log(nickName+ " Qual Classe você deseja Utilizar?")
 console.log("Mago ou Guerreiro")
 console.log()
-let classeHeroi = "mago"
+let classeHeroi = "Guerreiro"
 
 switch(classeHeroi){
 
-    case "mago":
+    case "Mago":
 
     vida = 200;
     mana = 200
@@ -22,7 +22,7 @@ switch(classeHeroi){
     console.log()
     break;
 
-    case "Guerreiro ":
+    case "Guerreiro":
 
     vida = 300;
     mana = 100;
@@ -39,5 +39,204 @@ switch(classeHeroi){
 
         break
 }
+
+let tutorial = false;
+
+if(tutorial === true){
+
+    let classe = "Guerreiro"
+
+    if(classe === "Mago"){
+
+        vida = 200;
+        mana = 200
+
+    console.log("Classe "+classe)
+    console.log("Vida :"+vida)
+    console.log("mana :"+mana)
+    console.log()
+
+
+    }else if(classe === "Guerreiro"){
+
+        vida = 300;
+        mana = 100;
+
+    console.log("Classe "+classe)
+    console.log("Vida :"+vida)
+    console.log("mana :"+mana)
+    console.log()
+   
+}
+
+console.log("Monstros com Xps ")
+
+console.log("EX: Monstro básicos : 500 XP")
+console.log("EX: Monstro Boss : 1000 XP")
+console.log()
+console.log("Fim do Tutorial")
+
+}else{
+
+    console.log("Bem Vindo ao Game")
+
+}
+
+console.log("Missao Inicial ")
+console.log();
+
+console.log("Enfrente seus MEDOS :");
+
+let monstro = 25;
+let xp = 0;
+
+for(let qat = 1;qat<=monstro;qat++){
+
+ xp+=200;
+
+console.log("Monstro "+qat+"/"+monstro+" restante")
+console.log("Nível Obtido "+xp)
+console.log()
+}
+
+console.log("Missão Concluida:")
+console.log()
+
+console.log("Missão Adicionada , Derrote os Fantasma")
+console.log();
+let desafio = true;
+
+if(desafio === true){
+
+    console.log("Derrote os Fantasma: ")
+
+    console.log()
+    let desafioFantasma = 7600
+
+    while(xp<desafioFantasma){
+
+        xp+=200;
+
+        console.log("Fantasma Derrotado")
+        console.log("Nível Obtido "+xp)
+        console.log()
+    }
+
+}
+console.log("Ultima missão, Desafie o BOSS'AAtrox'")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
